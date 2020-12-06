308328a33a3198983bc5c9a32ead66ee49156740
Callum Silcock <github@csi.lk>
2020-12-07 10:12:07 +1100

    feat: add index html / tsx for react entry
    time to start building

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react"
+import { render } from "react-dom"
+
+const rootEl = document.getElementById("pwc-todo-app")
+
+const App = () => <div>test</div>
+
+render(<App />, rootEl)
