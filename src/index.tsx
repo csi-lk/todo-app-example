08cbba2f0e19133b952124c4bdd5c98ba59a4fe1
Callum Silcock <github@csi.lk>
2020-12-07 10:31:27 +1100

    feat: basic reset and the system font stack

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import React from "react"
 import { render } from "react-dom"
 
+import "./index.css"
+
 const rootElement = document.querySelector("#pwc-todo-app")
 
-const App = (): React.ReactElement => <div>test</div>
+const App = (): React.ReactElement => (
+  <div>
+    <h1>Todo App</h1>
+  </div>
+)
 
 render(<App />, rootElement)
