640e0ae997203a5f671f45be0c8246748206192b
Callum Silcock <github@csi.lk>
2020-12-07 10:10:45 +1100

    build: add webpack and typescript config for building
    plus webpack dev server

---
+++ webpack.config.ts
@@ -0,0 +1,52 @@
+import { resolve } from "path"
+import { CheckerPlugin } from "awesome-typescript-loader"
+import HtmlWebpackPlugin from "html-webpack-plugin"
+
+import merge from "webpack-merge"
+import webpack from "webpack"
+
+const commonConfig = {
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  context: resolve(__dirname, "./src"),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["awesome-typescript-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CheckerPlugin(),
+    new HtmlWebpackPlugin({ template: "index.html.ejs" }),
+  ],
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+}
+
+const devConfig = merge(commonConfig, {
+  mode: "development",
+  entry: [
+    "react-hot-loader/patch", // activate HMR for React
+    "webpack-dev-server/client?http://localhost:8080", // bundle the client for webpack-dev-server and connect to the provided endpoint
+    "webpack/hot/only-dev-server", // bundle the client for hot reloading, only- means to only hot reload for successful updates
+    "./index.tsx", // the entry point of our app
+  ],
+  //   devServer: {
+  //     hot: true, // enable HMR on the server
+  //   },
+  //   devtool: "cheap-module-eval-source-map",
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // enable HMR globally
+  ],
+})
+
+module.exports = devConfig
