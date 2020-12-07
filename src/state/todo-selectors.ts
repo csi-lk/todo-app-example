b92ddfbdb42e19f43977201c5af42a75ad16e99a
Callum Silcock <github@csi.lk>
2020-12-07 15:01:39 +1100

    feat(state): add get total tasks selector

--- src/state/todo-selectors.ts
+++ src/state/todo-selectors.ts
@@ -2,6 +2,9 @@
 import { useMemo } from "react"
 import { TODO_STATE } from "./todo-state"
 
+export const getOpenTodos = (state: TODO_STATE): number =>
+  useMemo(() => Object.keys(state.todoItems).length, [state.todoItems])
+
 export const getCompletedTodos = (state: TODO_STATE): number =>
   useMemo(() => Object.keys(state.completedItems).length, [
     state.completedItems,
