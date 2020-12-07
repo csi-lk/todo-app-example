17a21ba04fd38b4d8e3c744c9c2921b2573745e9
Callum Silcock <github@csi.lk>
2020-12-07 11:33:43 +1100

    feat(task): props for todo text and whether checked or not

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -2,13 +2,24 @@ import React from "react"
 
 import styles from "./task.css"
 
-const Task = (): React.ReactElement => (
+const Task = ({
+  checked = false,
+  todoText,
+}: {
+  checked: boolean
+  todoText: string
+}): React.ReactElement => (
   <div className={styles.task}>
     <label>
-      <input type="checkbox" className={styles.checkbox} />
+      <input type="checkbox" className={styles.checkbox} checked={checked} />
       <div className={styles.fakeCheckbox} />
     </label>
-    <input type="text" className={styles.input} value="test data" />
+    <input
+      type="text"
+      className={styles.input}
+      value={todoText}
+      placeholder="Create a new todo item"
+    />
   </div>
 )
 
