641fbfe2b98c599b4971b40c71a11f5e4364747a
Callum Silcock <github@csi.lk>
2020-12-07 15:32:05 +1100

    feat: sort by todo item text by default

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React, { useReducer } from "react"
 import todoReducer from "./state/todo-reducer"
 import { initialState } from "./state/todo-state"
 import { TODO_ACTIONS } from "./state/todo-actions"
-import { getCompletedTodos, getOpenTodos } from "./state/todo-selectors"
+import {
+  getCompletedTodos,
+  getOpenTodos,
+  getOpenTodoIdsSorted,
+} from "./state/todo-selectors"
 import Task from "./components/task/Task"
 import TaskCount from "./components/task-count/TaskCount"
 import Header from "./components/header/Header"
@@ -13,7 +17,7 @@ const App = (): React.ReactElement => {
   return (
     <div>
       <Header openTodos={getOpenTodos(state)} />
-      {Object.keys(state.todoItems).map((id) => (
+      {getOpenTodoIdsSorted(state).map((id) => (
         <Task
           key={id}
           id={id}
